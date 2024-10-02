38388f7111b9e3d066bbfbbf6038886be944a4cb
KumaCod3 <kumacod3@proton.me>
2024-10-02 10:18:16 +0200

    sistemato testo?

--- Assets/mennu/Bottone.cs
+++ Assets/mennu/Bottone.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Bottone : MonoBehaviour
+public class Bottone: MonoBehaviour
 {
-    public GameObject dropD;
-    public int indice=0;
+	public GameObject dropD;
+	public int indice = 0;
+	public GameObject desc;
 
- public void OnClick()
-    {
-        indice = dropD.GetComponent<indice>().getIndex();
-        PlayerPrefs.SetInt("materiale", indice);
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
-            
-
-    }
+	public void OnClick()
+	{
+		indice = dropD.GetComponent<indice>().getIndex();
+		PlayerPrefs.SetInt("gioco", indice);
+		desc.SetActive(true);
+		desc.GetComponent<ControllerDescrizioni>().setGame(indice);
+		gameObject.SetActive(false);
+	}
 }
---
+++ Assets/mennu/ControllerDescrizioni.cs
@@ -0,0 +1,53 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerDescrizioni: MonoBehaviour
+{
+	string[] titoli;
+	string[] descrizioni;
+	int indice = 0;
+	public TextMeshProUGUI tits;
+	public TextMeshProUGUI des;
+	public Button carica;
+
+	void Start()
+	{
+		carica = gameObject.transform.GetChild(2).GetComponent<Button>();
+		titoli = new string[6];
+		descrizioni = new string[6];
+
+		titoli[0] = "";
+		descrizioni[0] = "";
+
+		titoli[1] = "My Little Packman";
+		descrizioni[1] = "To move you can use both arrows and WASD, each yellow ball gives 10 points and a few seconds, each red ball takes " +
+			"away ten points and a life. If you get 5 yellow balls, a red cube will appear which gives 50 points and a life, but you " +
+			"have to hurry!It will only stay for 10 seconds. The blue buttons in the corners take a life from you, but in return they make " +
+			"the walls disappear for a few seconds! ESC and DEL will lose the game!";
+
+		titoli[2] = "My Little DOOM";
+		descrizioni[2] = "To move you can use WASD, each GOLD target gives 15 points, each PURPLE target gives 2 points, each BLUE " +
+			"target takes away 20 points, and three seconds, each miss takes away 3 seconds. How many points can you score in 60 seconds ??? " +
+			"Pressing ESC will make you lose the game!";
+
+		titoli[3] = "pipino";
+		descrizioni[3] = "bla bla bla";
+
+		titoli[4] = "My Little TICtacToe";
+		descrizioni[4] = "";
+
+		titoli[5] = "venditore";
+		descrizioni[5] = "bla bvla kga akugh";
+
+	}
+
+	public void setGame(int x)
+	{
+		indice = x;
+		tits.SetText(titoli[x]);
+		des.SetText(descrizioni[x]);
+
+		//carica.
+	}
+}
